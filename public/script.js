7dea8f878cc221c25d2cf8260e602c7902db55d7
Maitann <hiagomaitan@gmail.com>
2023-04-28 01:10:47 -0300

    add server.js

---
+++ public/script.js
@@ -0,0 +1,16 @@
+function onOff() {
+    document
+        .querySelector("div#modal")
+        .classList
+        .toggle("hide")
+
+    document
+        .querySelector("body")
+        .classList
+        .toggle("hideScroll")
+
+     document
+        .querySelector("#modal")
+        .classList
+        .toggle("addScroll")
+}
---
+++ server.js
@@ -0,0 +1,19 @@
+//usei o express para criar e configurar meu servidor
+const express = require('express')
+const server = express()
+
+//configurar arquivos estaticos (css, scripts, imagens)
+server.use(express.static("public"))
+
+// criei uma rota "/"
+// e capturo o pedido do cliente para responder
+server.get("/", function(req, res){
+    return res.sendFile(__dirname + "/index.html")
+})
+
+server.get("/ideias", function(req, res){
+    return res.sendFile(__dirname + "/ideas.html")
+})
+
+// liguei meus ervidor na porta 3000
+server.listen(3000)
